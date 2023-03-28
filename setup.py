ea762da137f22cef8c55f529b23e3f6d160bcb78
gauenk <kent.gauen@gmail.com>
2023-03-28 10:52:17 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+# from distutils.core import setup
+import os
+import stat
+import shutil
+import platform
+import sys
+import site
+import glob
+
+
+# -- file paths --
+long_description="""Unsup Training with Video"""
+setup(
+    name='frame2frame',
+    version='100.100.100',
+    description='Frame2Frame Training Wrap',
+    long_description=long_description,
+    url='https://github.com/gauenk/apsearch',
+    author='Kent Gauen',
+    author_email='gauenk@purdue.edu',
+    license='MIT',
+    keywords='neural network',
+    install_requires=[],
+    package_dir={"": "lib"},
+    packages=find_packages("lib"),
+)
